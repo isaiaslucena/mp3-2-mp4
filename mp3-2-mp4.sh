91b696d8b39685a3574d0f8c73a17e5a44376329
isaiasneto <isaiasneto@gmail.com>
2017-10-22 22:20:21 -0200

    commited at 22/10/2017_22:20:21

--- mp3-2-mp4.sh
+++ mp3-2-mp4.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 OIFS="$IFS"
 IFS=$'\n'
@@ -7,7 +7,6 @@ function convtrack() {
 	cover=$(ls -S ${1} | egrep -i ".jpg|.jpeg|.png" | head -n 1)
 	for track in $(ls ${1} | grep -i ".mp3") ; do
 		trackname=$(echo ${track} | awk -F ".mp3" -F ".MP3" '{print $1}')
-		#ffmpeg -i ${track} -f image2 -loop 1 -i ${cover} -r 15 -s 640x480 -c:a libmp3lame -c:v libx264 -crf 18 -tune stillimage -preset fast conv/${trackname}.mp4
 		ffmpeg -loop 1 -i ${1}${cover} -i ${1}${track} -shortest -s 640x480 -c:a copy -tune stillimage -preset veryfast -y ${1}"conv/"${trackname}".mp4"
 	done
 }
@@ -18,13 +17,8 @@ if [[ -z "${cdirs}" ]] ; then
 	convtrack
 else
 	for cdir in ${cdirs} ; do
-		#cd ${dir}
-
 		mkdir -p ${cdir}"/conv"
-
 		convtrack "${cdir}"
-
-		#cd ..
 	done
 fi
 
